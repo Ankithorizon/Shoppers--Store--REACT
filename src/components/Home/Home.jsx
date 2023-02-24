dbd7c6954259dd8f427143f5d6f7af29385045bb
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2023-02-24 12:02:54 -0600

    login-wip2

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { Card, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { useNavigate } from "react-router-dom";
+
+import "./style.css";
+
+const Home = () => {
+  let navigate = useNavigate();
+
+  return (
+    <div className="mainContainer">
+      <h3>home-page</h3>
+
+      <p></p>
+      <div className="innerContent">
+        <Container>
+          <Row>
+            <Col className="innerContent">
+              <Card className="cardStyle" style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Products</Card.Title>
+                  <Card.Text>Products</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="innerContent">
+              <Card className="cardStyle" style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Shopping</Card.Title>
+                  <Card.Text>Shopping</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/services/authentication.service.js
@@ -0,0 +1,35 @@
+import http from "../axios/authentication-http-common";
+
+export default class AuthenticationService {
+  login = async (data) => {
+    return await http.post(`/login`, data);
+  };
+
+  getCurrentUser = () => {
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    return currentUser !== null ? currentUser : null;
+  };
+
+  getToken = () => {
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    return currentUser !== null ? currentUser.token : null;
+  };
+
+  getCurrentUserRole = () => {
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    return currentUser !== null ? currentUser.role : null;
+  };
+
+  getCurrentUserName = () => {
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    return currentUser !== null ? currentUser.userName : null;
+  };
+
+  logout = () => {
+    localStorage.removeItem("currentUser");
+  };
+
+  register = async (data, myRole) => {
+    return await http.post(`/register/${myRole}`, data);
+  };
+}
